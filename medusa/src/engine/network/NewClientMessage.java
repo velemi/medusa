fc731753fbad313c3c7403b6b28153f6cc2e5294
velemi <velemi.sin@gmail.com>
2016-11-10 02:19:05 -0500

    idiot file didn't commit

---
+++ medusa/src/engine/network/NewClientMessage.java
@@ -0,0 +1,20 @@
+package engine.network;
+
+import engine.gameObjects.PlayerObject;
+
+public class NewClientMessage extends NetworkMessage
+{
+	private static final long serialVersionUID = -277803654717665590L;
+	
+	private PlayerObject player;
+	
+	public NewClientMessage(PlayerObject player)
+	{
+		this.player = player;
+	}
+	
+	public PlayerObject getPlayer()
+	{
+		return this.player;
+	}
+}
